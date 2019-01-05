d1090fb21fe67ac353d64d31525f2c40bff2e886
kici8 <artusi.daniele@gmail.com>
2019-01-06 00:13:56 +0100

    Pre refactoring css

--- asset/js/app.js
+++ asset/js/app.js
@@ -2,10 +2,13 @@
 {
 DOMStings = {
   nav: {
+    logo: "#header__logo",
+    logoName: "#header__logo__name",
     menu: "#nav",
     trigger: "#nav--trigger",
     toggle: {
-      active: "active" // no class selector
+      active: "active", // no class selector
+      show: "show" // no class selector
     }
   }
 };
@@ -18,7 +21,31 @@ toggleClass = function(t, classToToggle) {
   element.classList.toggle(classToToggle);
 };
 
+addClass = function(t, classToToggle) {
+  var element = document.querySelector(t);
+  if (!element.classList.contains(classToToggle)) {
+    element.classList.add(classToToggle);
+  }
+};
 
+removeClass = function(t, classToToggle) {
+  var element = document.querySelector(t);
+  element.classList.remove(classToToggle);
+};
+
+
+
+// Header on scroll position
+var transformHeaderOnScroll = function() {
+  var y = window.scrollY;
+  if (y >= 200) {
+    removeClass (DOMStings.nav.logoName, DOMStings.nav.toggle.show);
+    removeClass (DOMStings.nav.logo, DOMStings.nav.toggle.show);
+  } else {
+    addClass (DOMStings.nav.logoName, DOMStings.nav.toggle.show);
+    addClass (DOMStings.nav.logo, DOMStings.nav.toggle.show);
+  }
+};
 
 /////////////////////////
 // Event Listener
@@ -31,3 +58,8 @@ document
     toggleClass(DOMStings.nav.menu, DOMStings.nav.toggle.active);
   });
 }
+
+
+window.addEventListener("scroll", transformHeaderOnScroll);
+
+
